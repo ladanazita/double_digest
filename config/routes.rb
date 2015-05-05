7714284e82ae4133516ea8145d83cd7fbaa9fe4a
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-04 20:27:05 -0700

    uncommented match routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  # match 'auth/:provider/callback', to: 'sessions#create', via [:get, :post]
-  # match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  # match 'signout', to: 'sessions#destroy', as:'signout', via: [:get, :post]
+  match 'auth/:provider/callback', to: 'sessions#create', via [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as:'signout', via: [:get, :post]
 
     resources :user
       resources :trend
