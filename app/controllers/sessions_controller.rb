8dc7f7c03383601106a2623bf2ca66f58c6cc332
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-04 20:29:21 -0700

    defined create and destroy sessions for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,10 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id]= user.id
+    redirect_to root_url
+  end
+  def destroy
+    session[:user_id]=nil
+  end
 end
