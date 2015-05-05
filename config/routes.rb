9e41cfb475e659b13af5402f38a37e3eaa77c4a1
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-04 20:20:01 -0700

    configured routes wiht match for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
   root 'welcome#index'
-
-  resources :user
-  resources :trend
-  resources :video
-  resources :comment
-
+  match 'auth/:provider/callback', to: 'sessions#create', via [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as:'signout', via: [:get, :post]
+    resources :user
+      resources :trend
+        resources :video
+          resources :comment
  end
