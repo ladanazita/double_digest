dbe4ba4588b536dc1a3196ab1592466e2e1867e7
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-07 14:00:08 -0700

    added hashtag column to trends

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507205534) do
+ActiveRecord::Schema.define(version: 20150507205738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150507205534) do
   create_table "trends", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "hashtags"
   end
 
   create_table "users", force: :cascade do |t|
