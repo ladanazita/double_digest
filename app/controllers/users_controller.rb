4ed4dc489974b5155523351554602a14377ffafa
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-09 22:20:28 -0700

    created form for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to users_path
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to('/signup')
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe UsersController, type: :controller do
   describe "POST #create" do
     context "with valid attributes" do
       it "saves the new user in the database" do
-        expect{post :create, user: valid_attributes}.to change(User, :count).by(1)
+        expect{post :create, user: attributes_for(:user)}.to change(User, :count).by(1)
       end
       it "redirects to the home page"
     end
