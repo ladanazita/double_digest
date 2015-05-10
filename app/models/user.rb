e68f62780ec08049f43771ca1c6973e00b6de4cf
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-09 21:26:44 -0700

    user has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :comments
+  has_secure_password
   validates :name, uniqueness: {scope: :name}, presence: true
   validates :email, uniqueness: {scope: :email}, presence: true
   validates :password_digest, presence: true
