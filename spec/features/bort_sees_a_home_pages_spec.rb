a8498f9148bf52d961bfed249e55a2b805547821
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-09 21:25:24 -0700

    passed test view login

--- spec/features/bort_sees_a_home_pages_spec.rb
+++ spec/features/bort_sees_a_home_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "BortSeesAHomePages", type: :feature do
   scenario "he sees a login button" do
     visit '/'
-    expect(page).to have_css("h1")
-    expect(page).to have_content("sign in")
+    expect(page).to have_css("li")
+    expect(page).to have_content("Sign In")
   end
 end
