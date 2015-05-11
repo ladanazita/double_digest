114101567fa5cd65108c577d6310c393f4ab5623
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-10 17:56:11 -0700

    added a helper method for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
+  helper_method :current_user
   # before_filter :load_tweets
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+      return @current_user
+    else
+      return nil
+    end
+  end
+
 end
