705dbf2fe65cc1f0f0aa1917c4b06064d30eaefa
Your Name <alexdinari@roadhouseapps.com>
2015-05-12 16:51:53 -0700

    Added trends.rb

--- app/models/trend.rb
+++ app/models/trend.rb
@@ -1,3 +1,11 @@
 class Trend < ActiveRecord::Base
   has_many :videos
+
+  def self.refresh
+		tweets = $twitter.home_timeline.take(40)
+		@tweets.each do |t|
+			t.hashtags.each do |h| 
+				h.text
+
+  end
 end
