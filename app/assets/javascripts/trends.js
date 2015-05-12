2c276a2101456f163ccc58b1627842bc7ef63009
Your Name <alexdinari@roadhouseapps.com>
2015-05-12 15:54:24 -0700

    stopping with this issue for the moment to add the background worker gem

--- app/assets/javascripts/trends.js
+++ app/assets/javascripts/trends.js
@@ -1,1 +1,6 @@
+$(document).ready(function(){
 
+		// This is doing a GET request to our smoothies endpoint
+		$.post('http://localhost:3000/api/trends', function(hashtags){
+			
+	});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
-  before_filter :load_tweets
 end
