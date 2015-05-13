ca6f721456c928165c0af52b23c17d2fb4a4150c
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-13 13:22:41 -0700

    defining login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,8 +13,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
-      params.require(:user).permit(:name, :email,:password)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
