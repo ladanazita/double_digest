7fa56d7838b53d8a6d553435e450edeca9eed71a
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-13 17:44:32 -0700

    added login view, cleaned up routes, removed wlecome index, route to application view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'application#index'
   # get 'trends/index'
   # get 'trends/show'
   get '/signup' => 'users#new'
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/id' => 'users#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'session#destroy'
+  get '/signout' => 'session#destroy'
 
 
     resources :user
