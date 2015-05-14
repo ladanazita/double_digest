d852c836a70af6247460bb71c0b20dcf53975a94
Your Name <alexdinari@roadhouseapps.com>
2015-05-13 17:15:03 -0700

    Trying to get shit working

---
+++ app/jobs/twitter_worker_job.rb
@@ -0,0 +1,17 @@
+class TwitterWorkerJob < ActiveJob::Base
+  queue_as :default
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+  include ApplicationHelper
+
+  recurrence { hourly(4) }
+
+  # def perform
+  #   tweets = load_tweets
+  #   tweets.each do |t|
+  #     t.hashtags.each do |h| 
+  #       Trend.create(hashtags: h.text, created_at: Time.now)
+  #     end
+  #   end
+  # end 
+end
