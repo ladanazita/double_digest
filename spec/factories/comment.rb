9bc22f86efc58e24f7c78402a8e4b82b106a8c8b
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-14 15:45:02 -0700

    created comment factory to test

---
+++ spec/factories/comment.rb
@@ -0,0 +1,11 @@
+require 'Faker'
+require 'factory_girl_rails'
+
+FactoryGirl.define do
+  factory :comment do |f|
+    f.body {Faker::Lorem.paragraph}
+  end
+  factory :invalid_comment, parent :comment do |f|
+    f.body nil
+  end
+end
