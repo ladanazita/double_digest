37b68a8143fef0dbae33bc4e18bdceca97f57afe
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-13 18:45:14 -0700

    resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -19,6 +20,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.permit(:name, :email, :password, :password_confirmation)
     end
 end
