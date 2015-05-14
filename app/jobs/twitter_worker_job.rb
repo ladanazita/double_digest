cf8278a22c7e48a929e9c1b6cd736a354506bca8
Your Name <alexdinari@roadhouseapps.com>
2015-05-14 14:24:26 -0700

    Pre Modal implementation

--- app/jobs/twitter_worker_job.rb
+++ app/jobs/twitter_worker_job.rb
@@ -4,13 +4,15 @@ class TwitterWorkerJob < ActiveJob::Base
   include Sidetiq::Schedulable
   include ApplicationHelper
 
+  # SETS UP THE TIME INTERVAL FOR SIDEKIQ + SIDETIQ
   recurrence { hourly(2) }
 
+  # MY PERFORM ACTION FOR SAVING DATA FROM TWTIITERS API TO OUR DB
   def perform
     tweets = load_tweets
     tweets.each do |t|
       t.hashtags.each do |h| 
-        Trend.create(hashtags: h.text, created_at: Time.now)
+          Trend.create(hashtags: h.text, created_at: Time.now)
       end
     end
   end 
