0a9bb693505d16701899e3e14c6bf00d46773614
Your Name <alexdinari@roadhouseapps.com>
2015-05-14 17:50:48 -0700

    Calvins youtube logic is working

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,18 @@ Rails.application.routes.draw do
   post '/signup' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
 
-      namespace :api do
-        resources :trends
-          resources :video
-            resources :comment
+  namespace :api do
+    namespace :v1 do
+      resources :trends do
+        resources :videos do
+          resources :comment
+        end
+      end
     end
+  end
+  
 
   # match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   # match '/auth/failure', to: redirect('/'), via: [:get, :post]
   # match '/signout', to: 'sessions#destroy', as:'signout', via: [:get, :post]
- end
+end
