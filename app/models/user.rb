beb3ef01a1b179f4f31a6d0e6553ec2cc9d51fb4
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-15 16:25:49 -0700

    this closes #97

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many :comments
   has_secure_password
   validates :name, uniqueness: {scope: :name}, presence: true
+  # validates the email using regex
   validates :email, uniqueness: {scope: :email, case_sensitive: false},length: {minimum: 6}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, presence: true
   validates :password_digest, presence: true
 end
