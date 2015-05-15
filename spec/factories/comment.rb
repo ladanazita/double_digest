9c6b0977dbc083e13f9b6a075a4a6a60315250c1
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-14 18:10:48 -0700

    new error, not finding trend ID to comment

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -4,6 +4,7 @@ require 'factory_girl_rails'
 FactoryGirl.define do
   factory :comment do |f|
     f.body {Faker::Lorem.paragraph}
+    f.trend_id{1}
   end
   factory :invalid_comment, parent: :comment do |f|
     f.body nil
