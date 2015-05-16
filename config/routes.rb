e252c466c4f803d391af92db5f6fd5543aaaea96
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-16 16:57:18 -0700

    created routes for facebook login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   post '/signup' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
-
+  # FACEBOOK LOGIN
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as:'signout', via: [:get, :post]
   # ALL OTHER ROUTES
   namespace :api do
     namespace :v1 do
