4830437599cefb3963f8e78026419df496d73d71
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-17 15:21:44 -0700

    added new action and tested it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     user=User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,6 +4,14 @@ require 'spec_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  describe "POST create" do
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe "DELETE #destroy" do
+    it "returns http success" do
+      get :new
 
 end
