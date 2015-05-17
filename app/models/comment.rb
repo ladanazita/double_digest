2740a3745983b97c4b0465107a53c10e77ad89d6
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-16 23:16:06 -0700

    four tests for the comment model passed, this closes #23

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,10 @@
 class Comment < ActiveRecord::Base
+
   belongs_to :trend
   belongs_to :user
+
   validates :body, presence: true
   validates :user_id, presence: true
   validates :trend_id, presence: true
+
 end
--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -2,11 +2,15 @@ require 'Faker'
 require 'factory_girl_rails'
 
 FactoryGirl.define do
+
   factory :comment do |f|
     f.body {Faker::Lorem.paragraph}
     f.user {create(:user)}
+    f.trend {create(:trend)}
   end
+
   factory :invalid_comment, parent: :comment do |f|
     f.body nil
   end
+
 end
